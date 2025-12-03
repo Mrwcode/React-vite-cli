9545b416b18864df11eef4fe8a83efa62853ae14
gavin.wang <gavin.wang@cloudwise.com>
2025-12-03 14:51:45 +0800

    配置文件

--- configs/env.config.js
+++ configs/env.config.js
@@ -2,12 +2,12 @@
  * Author: Gavin.wang
  * Date: 2025-11-28 18:16:58
  * LastEditors: Gavin.wang
- * LastEditTime: 2025-12-02 17:12:37
+ * LastEditTime: 2025-12-02 17:27:38
  * FilePath: /react-vite-cli/configs/env.config.js
  * Description:
  */
 const ENV = {
-  routeMode: 'online',
+  routeMode: 'local', //local | online
   buildTimeStamp: '2025-12-01 14:55:38',
   branchName: 'develop',
 };
